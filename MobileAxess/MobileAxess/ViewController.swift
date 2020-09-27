22ed1486b37fe208247714067349d7bca393d67c
anuj.vats <anuj.vats@accenture.com>
2020-09-27 15:31:42 +0530

    Xcode Project Creation

---
+++ MobileAxess/MobileAxess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileAxess
+//
+//  Created by Anuj Vats on 27/09/20.
+//  Copyright © 2020 Anuj Vats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
