01d5cadfc2795ac6c8419bbe521d21da446532ac
anuj.vats <anuj.vats@accenture.com>
2020-09-27 15:52:31 +0530

    Add new Groups to project structure

---
+++ MobileAxess/MobileAxess/Sections/Home/ViewController/MADetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MADetailViewController.swift
+//  MobileAxess
+//
+//  Created by Anuj Vats on 27/09/20.
+//  Copyright © 2020 Anuj Vats. All rights reserved.
+//
+
+import UIKit
+
+class MADetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MobileAxess/MobileAxess/Sections/Home/ViewController/MAHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MAHomeViewController.swift
+//  MobileAxess
+//
+//  Created by Anuj Vats on 27/09/20.
+//  Copyright © 2020 Anuj Vats. All rights reserved.
+//
+
+import UIKit
+
+class MAHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
